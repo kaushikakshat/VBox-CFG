821b92170b27d47d248f0425a5b56504b839fc81
kaushikakshat <kaushikakshat.work@gmail.com>
2024-04-15 13:34:03 +0530

    update

---
+++ README.md
@@ -0,0 +1,32 @@
+# VBox CFG Extractor
+
+VBox CFG Extractor is a Python tool designed to extract the Control Flow Graph (CFG) from VirtualBox (VBox) logs. 
+
+## Installation
+
+You can install the tool using pip:
+
+```bash
+git clone https://github.com/kaushikakshat/VBox-CFG.git
+```
+
+## Usage
+
+To use VBox CFG Extractor, simply run the following command:
+
+1. Open the graph.py file.
+2. Replace the file name with the file you'd like to make CFG from in line 15.
+3. ```python3 graph.py```
+
+## Example
+
+```bash
+python3 graph.py
+```
+
+This will generate a CFG visualization file in the PDF.
+
+## Dependencies
+
+- Python 3.x
+- Graphviz
--- graph.py
+++ graph.py
@@ -1,111 +1,3 @@
-# from graphviz import Digraph
-# import re
-
-# dot = Digraph()
-
-# # # Add nodes to the graph
-# # dot.node('A', 'Node A')
-# # dot.node('B', 'Node B')
-# # dot.node('C', 'Node C')
-
-# # # Add edges to the graph
-# # dot.edge('A', 'B')
-# # dot.edge('B', 'C')
-# # dot.edge('C', 'A')
-
-# # # Specify the output file format (optional)
-# # dot.format = 'png'
-
-# # # Render the graph to a file
-# # dot.render('example_graph', view=True)
-
-# # Alternatively, to view the graph without saving it:
-# # dot.view()
-
-
-
-# level1 = []
-# level2 = []
-# level3 = []
-# level4 = []
-# level5 = []
-# level6 = []
-# level7 = []
-
-# with open("Windows-11-VBox-Logs", "r") as logs:
-    
-#     pattern = r'\[(.*?)\]'
-
-#     for line in logs.readlines():
-#         if "level 1" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level1.append(match)
-#         elif "level 2" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level2.append(match)
-#         elif "level 3" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level3.append(match)
-#         elif "level 4" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level4.append(match)
-#         elif "level 5" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level5.append(match)
-#         elif "level 6" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level6.append(match)
-#         elif "level 7" in line:
-#             matches = re.findall(pattern, line)
-#             for match in matches:
-#                 level7.append(match)
-
-# nodes = level1 + level2 + level3 + level4 + level5 + level6 + level7
-
-# # for node in level2:
-# #     print(node)
-
-# for node in level1:
-#     dot.edge("/", node)
-#     for node2 in level2:
-#         if node in node2:
-#             dot.edge(node, node2)
-#         for node3 in level3:
-#             if node2 in node3:
-#                 dot.edge(node2, node3)
-#             for node4 in level4:
-#                 if node3 in node4:
-#                     dot.edge(node3, node4)
-#                 for node5 in level5:
-#                     if node4 in node5:
-#                         dot.edge(node4, node5)
-#                     for node6 in level6:
-#                         if node5 in node6:
-#                             dot.edge(node5, node6)
-#                         for node7 in level7:
-#                             if node6 in node7:
-#                                 dot.edge(node6, node7)
-                
-        
-
-# for node in level2:
-#     pass
-
-# # dot.format = 'png'
-
-# # # Render the graph
-# dot.render('example_graph', cleanup=True)
-
-# # # Alternatively, to view the graph without saving it:
-# # dot.view()
-
-
 from graphviz import Digraph
 import re
 
@@ -119,6 +11,7 @@ level5 = []
 level6 = []
 level7 = []
 
+# Chnage this file name with your file name 
 with open("Windows-11-VBox-Logs", "r") as logs:
     pattern = r'\[(.*?)\]'
     
